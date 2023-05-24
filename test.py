b46b5956c974e835e876491b5b0cf3a8b0e95b2f
0xspringtime <0xspringtime@protonmail.com>
2023-05-24 09:36:28 -0400

    commit

--- test.py
+++ test.py
@@ -41,4 +41,36 @@ def scaled_dot_product_attention(q, k, v, mask):
     output = torch.matmul(attention_weights, v)
     return output, attention_weights
 
+#multihead attention layer
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+        assert d_model % self.num_heads == 0
+        self.depth = d_model // self.num_heads
+        self.wq = nn.Linear(d_model, d_model)
+        self.wk = nn.Linear(d_model, d_model)
+        self.wv = nn.Linear(d_model, d_model)
+        self.dense = nn.Linear(d_model, d_model)
+
+    def split_heads(self, x, batch_size):
+        x = x.view(batch_size, -1, self.num_heads, self.depth)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, v, k, q mask):
+        batch_size = q.size(0)
+        q = self.wq(q)
+        k = self.wk(k)
+        v = self.wv(v)
+        q = self.split_heads(q, batch_size)
+        k = self.split_heads(k, batch_size)
+        v = self.split_heads(v, batch_size)
+        scaled_attention, attention_weights = scaled_dot_product_attention(q, k, v, mask)
+        scaled_attention = scaled_attention.permute(0, 2, 1, 3).contiguous()
+        concat_attention = scaled_attention.view(batch_size, -1, self.d_model)
+        output = self.dense(concat_attention)
+        return output, attention_weights
+
+#point-wise feed-forward network
