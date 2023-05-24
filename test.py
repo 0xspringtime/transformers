7fe9f30a10cff8b596f6121e2cf627e1bfec4623
0xspringtime <0xspringtime@protonmail.com>
2023-05-24 09:00:02 -0400

    commit

--- test.py
+++ test.py
@@ -12,4 +12,32 @@ input_vocab_size = 10000
 maximum_position_encoding = 1000
 dropout_rate = 0.1
 
+#positional encoding
+
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / torch.pow(10000, (2 * (i // 2)) / torch32(d_model))
+    return pos * angle_rates
+
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(torch.arange(position)[:, np.newaxis],
+                            torch.arange(d_model)[np.newaxis, :],
+                            d_model)
+    angle_rads[:, 0::2] = torch.sin(angle_rads[:, 0::2])
+    angle_rads[:, 1::2] = torch.cos(angle_rads[:, 1::2])
+    pos_encoding = angle_rads[np.newaxis, ...]
+    return pos_encoding
+
+#scaled dot-product attention function
+
+def scaled_dot_product_attention(q, k, v, mask):
+    matmul_qk = torch.matmul(q, k.transpose(-2, -1))
+    dk = torch.tensor(k.size(-1), dtype=torch.float32)
+    scaled_attention_logits = matmul_qk / torch.sqrt(dk)
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    attention_weights = torch.softmax(scaled_attention_logits, dim=-1)
+    output = torch.matmul(attention_weights, v)
+    return output, attention_weights
+
 
