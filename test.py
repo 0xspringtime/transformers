a25627613a5cb2a97b8f5ff42b1865813a7432db
0xspringtime <0xspringtime@protonmail.com>
2023-05-24 10:08:27 -0400

    commit

--- test.py
+++ test.py
@@ -42,6 +42,7 @@ def scaled_dot_product_attention(q, k, v, mask):
     return output, attention_weights
 
 #multihead attention layer
+#MultiHeadAttention(Q, K, V) = Concat(head_1, ..., head_h)W_O
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_model, num_heads):
@@ -59,7 +60,7 @@ class MultiHeadAttention(nn.Module):
         x = x.view(batch_size, -1, self.num_heads, self.depth)
         return x.permute(0, 2, 1, 3)
 
-    def forward(self, v, k, q mask):
+    def forward(self, v, k, q, mask):
         batch_size = q.size(0)
         q = self.wq(q)
         k = self.wk(k)
@@ -74,3 +75,58 @@ class MultiHeadAttention(nn.Module):
         return output, attention_weights
 
 #point-wise feed-forward network
+#ReLU(x) = max(0, x)
+
+def point_wise_feed_forward_network(d_model, dff):
+    return nn.Sequential(
+        nn.Linear(d_model, dff),
+        nn.ReLU(),
+        nn.Linear(dff, d_model)
+    )
+
+#encoder layer
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(EncoderLayer, self).__init__()
+        self.mha = MultiHeadAttention(d_model, num_heads)
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+        self.layernorm1 = nn.LayerNorm(d_model, eps=1e-6)
+        self.layernorm2 = nn.LayerNorm(d_model, eps=1e-6)
+        self.dropout1 = nn.Dropout(rate)
+        self.dropout2 = nn.Dropout(rate)
+
+    def forward(self, x, mask):
+        attn_output, _ = self.mha(x, x, x, mask)
+        attn_output = self.dropout1(attn_output)
+        out1 = self.layernorm1(x + attn_output)
+        ffn_output = self.ffn1
+        ffn_output = self.dropout2(ffn_output)
+        out2 = self.layernorm2(out1 + ffn_output)
+        return out2
+
+#transformer encoder
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, maximum_position_encoding, rate=0.1):
+        super(TransformerEncoder, self).__init__()
+        self.d_model = d_model
+        self.num_layers = num_layers
+        self.embedding = nn.Embedding(input_vocab_size, d_model)
+        self.pos_encoding = positional_encoding(maximum_position_encoding, self.d_model)
+        self.enc_layers = nn.ModuleList([EncoderLayer(d_model, num_heads, dff, rate) for _ in range(num_layers)])
+        self.dropout = nn.Dropout(rate)
+
+    def forward(self, x, mask):
+        seq_len = x.size(1)
+        x = self.embedding(x)
+        x *= torch.sqrt(torch.tensor(self.d_model, dtype=torch.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+        x = self.dropout(x)
+        for i in range(self.num_layers):
+            x = self.enc_layers[i](x, mask)
+        return x
+
+#llm
+
+
